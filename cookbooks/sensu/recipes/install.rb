27cda6a68ba054bfd94b624898eddab06fcc6467
GIT Admin <prasad.hg@outlook.com>
2016-02-25 17:02:39 +0530

    server config done

--- cookbooks/sensu/recipes/install.rb
+++ cookbooks/sensu/recipes/install.rb
@@ -36,13 +36,13 @@ bash 'SSL' do
 end
 
 directory '/etc/rabbitmq/ssl' do
-  owner 'root'
-  group 'root'
-  mode '0755'
+ owner 'root'
+ group 'root'
+ mode '0755'
  action :create
 end
 
-bash 'copy_SSL' do 
+bash 'copy_SSL_Rabbitmq' do 
  code "cp /tmp/ssl_certs/sensu_ca/cacert.pem /tmp/ssl_certs/server/cert.pem /tmp/ssl_certs/server/key.pem /etc/rabbitmq/ssl"
 end
 
@@ -57,7 +57,7 @@ file '/etc/rabbitmq/rabbitmq.config' do
                    {fail_if_no_peer_cert,true}]}
     ]}
   ]. '
- mode '0755'
+ mode '0644'
  owner 'root'
  group 'root'
 end
@@ -66,6 +66,121 @@ service 'rabbitmq-server' do
  action  :restart 
 end
 
+execute 'adding RabbitMQ virtual host' do
+ command 'rabbitmqctl add_vhost /sensu; rabbitmqctl add_user sensu pass ; rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*" '
+end
 
+package 'redis-server' do
+ action :install
+end
+
+service 'redis-server' do
+ action [ :enable, :start ]
+end
+
+bash 'adding sources & keys to sensu' do
+ code "wget -q http://repos.sensuapp.org/apt/pubkey.gpg -O- | sudo apt-key add - ; echo 'deb http://repos.sensuapp.org/apt sensu main' | sudo tee -a /etc/apt/sources.list.d/sensu.list "
+end
+
+package 'sensu' do
+ action :install
+end
+
+package 'uchiwa' do
+ action :install
+end
+
+directory '/etc/sensu/ssl' do
+ owner 'root'
+ group 'root'
+ mode '0755'
+ action :create
+end
+
+bash 'SSL_copy_sensu' do
+ code "cp /tmp/ssl_certs/client/cert.pem /tmp/ssl_certs/client/key.pem /etc/sensu/ssl"
+end
+
+file '/etc/sensu/conf.d/rabbitmq.json' do
+ content '{
+  "rabbitmq": {
+    "ssl": {
+      "cert_chain_file": "/etc/sensu/ssl/cert.pem",
+      "private_key_file": "/etc/sensu/ssl/key.pem"
+    },
+    "host": "localhost",
+    "port": 5671,
+    "vhost": "/sensu",
+    "user": "sensu",
+    "password": "abc123"
+    }
+  }'
+ owner 'root'
+ group 'root'
+ mode '0644'
+ action :create
+end
+
+file '/etc/sensu/conf.d/redis.json' do
+ content '{
+  "redis": {
+    "host": "localhost",
+    "port": 6379
+   }
+  }'
+ owner 'root'
+ group 'root'
+ mode '0644'
+ action :create
+end
+
+file '/etc/sensu/conf.d/api.json' do
+ content '{
+  "api": {
+    "host": "localhost",
+    "port": 4567
+   }
+  }'
+ owner 'root'
+ group 'root'
+ mode '0644'
+ action :create
+end
+
+file '/etc/sensu/uchiwa.json' do
+ content '{
+    "sensu": [
+        {
+            "name": "Sensu",
+            "host": "localhost",
+            "ssl": false,
+            "port": 4567,
+            "path": "",
+            "timeout": 5000
+        }
+    ],
+    "uchiwa": {
+        "port": 3000,
+        "stats": 10,
+        "refresh": 10000
+    }
+  }'
+ owner 'root'
+ group 'root'
+ mode '0644'
+ action :create
+end
+
+service 'sensu-server'  do
+ action [ :enable, :start ]
+end
+
+service 'sensu-api' do
+ action [ :enable, :start ]
+end
+
+service 'uchiwa' do
+ action [ :enable, :start ]
+end
 
 
